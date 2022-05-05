fe8a4d54f614c2f75bbf92fc0ab4c1446db0b563
Daniel Walsh <walshy@cloudflare.com>
2022-05-05 18:28:27 +0100

    worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,7 +2,7 @@ interface Env {
   SECRET: string;
 }
 
-// Test 4
+// Test 5
 export default {
   async fetch(_: Request, env: Env): Promise<Response> {
     return new Response(`Did the secret work? Build ID: ${env.SECRET}`);
